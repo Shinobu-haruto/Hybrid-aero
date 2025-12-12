d216d9ccc65832b056f546eca8d31cfbd5f0b618
Shinobu-haruto <contacto.kamiharuto@gmail.com>
2025-12-11 19:47:13 -0600

    Crear setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+@echoff
+title Assistant Setup
+pause 6s
+echo --------------------
+echo "Please wait...."
+pause 5s
+mkdir $HOME$/.local/shared/themes/HybridAero
+cp ./ $HOME$/.local/shared/themes/HybridAero
+echo  "Parch..."
+sudo apt install picom
+cp ./parch/picom.conf $HOME$/.local/shared/
